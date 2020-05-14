87bd949ed818758beddfcf1e23def7d3ab4613ad
evitp <38795986+evitp@users.noreply.github.com>
2020-05-14 19:47:19 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,6 +2,6 @@
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('0.0.0.0', 8080))
     client.send("I am Evitas CLIENT<br>")
-    from_server = client.recv(4096)
+    from_server = client.recv(3333)
     client.close()
     print from_server
