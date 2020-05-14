118a1485cfa040e6fea1a4fb3b353a0c355a30b2
evitp <38795986+evitp@users.noreply.github.com>
2020-05-14 21:34:21 +0300

    Update UDPserver.py

--- UDPserver.py
+++ UDPserver.py
@@ -1,12 +1,21 @@
-   import socket
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 3333)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+while True:
+    print >>sys.stderr, '\nwaiting to receive message'
+    data, address = sock.recvfrom(4096)
     
-   UDP_IP = "127.0.0.1"
-   UDP_PORT = 5005
-   
-   sock = socket.socket(socket.AF_INET, 
-                        socket.SOCK_DGRAM) 
-   sock.bind((UDP_IP, UDP_PORT))
-   
-   while True:
-       data, addr = sock.recvfrom(1024) 
-       print "received message:", data
+    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    print >>sys.stderr, data
+    
+    if data:
+        sent = sock.sendto(data, address)
+        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
