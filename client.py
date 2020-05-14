3581adcc4e35b72c420b38d6ae7d686b6a7283f8
evitp <38795986+evitp@users.noreply.github.com>
2020-05-14 21:01:13 +0300

    Update client.py

--- client.py
+++ client.py
@@ -5,3 +5,37 @@
     from_server = client.recv(3333)
     client.close()
     print from_server
+
+socket_echo_client.py
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 5555)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+try:
+
+    # Send data
+    message = b'This is Evitas message for M2 assignment. It will be repeated.'
+    print('sending {!r}'.format(message))
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print('received {!r}'.format(data))
+
+finally:
+    print('closing socket')
+    sock.close()
+
