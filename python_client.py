bd22467b575f23be1b970a7c6dd54db029923026
evitp <38795986+evitp@users.noreply.github.com>
2020-05-14 21:05:11 +0300

    Update and rename client.py to python_client.py

--- python_client.py
+++ python_client.py
@@ -1,12 +1,4 @@
- import socket
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('0.0.0.0', 8080))
-    client.send("I am Evitas CLIENT<br>")
-    from_server = client.recv(3333)
-    client.close()
-    print from_server
-
-socket_echo_client.py
+#client.py
 
 import socket
 import sys
